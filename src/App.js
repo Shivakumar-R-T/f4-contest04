5d969d48fbe3abc0a11045e340f73b1e4d02465a
Shivakumar-R-T <112469085+Shivakumar-R-T@users.noreply.github.com>
2023-01-30 00:25:06 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import CustomRoutes from './Routing/CustomRoutes';
+
+
+function App() {
+  return (
+    <div className="App">
+    <CustomRoutes/>
+    
+
+    </div>
+  );
+}
+
+export default App;
